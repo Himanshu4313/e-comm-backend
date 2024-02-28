dcd75f5976ac269311d037093a9b8dac11adaf96
Himanshu4313 <hk225064@gmail.com>
2024-02-28 22:06:09 +0530

    admin user created

--- models/users.model.js
+++ models/users.model.js
@@ -1,12 +1,60 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
-const  Schema = mongoose.Schema;
+const Schema = mongoose.Schema;
 
 // Create the model class for a user, inheriting from mongoose's Document class
-let UserModel = new Schema({
+let UserModel = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name fields is mandatory"],
+      minLength: [5, "Name should be atleast 5 character"],
+    },
+
+    userId: {
+      type: String,
+      required: [true, "User Id fields  is mandatory"],
+      lowercase: true,
+      unique: true,
+    },
+
+    email: {
+      type: String,
+      required: [true, "Email is mandatory"],
+      lowercase: true,
+      unique: true,
+      trim : true
+    },
+
+    password: {
+      type: String,
+      required: [true, "Password fields are mandatory"],
+      minLength: [8],
+      maxLength: [10],
+      select: false, //hides this field while querying, used to hide passwords in responses , default is false ,
+    },
+
+    userType: {
+      type: String,
+      required: [true, "This fields is mandatory"],
+      enum: ["ADMIN", "CUSTOMER"],
+      default: "CUSTOMER",
+    },
+  },
+  { timestamps: true, versionKey: false }
+);
+
+
+// password save as encrypted in database .
+UserModel.pre("save", async function (next) {
+    if (!this.isModified("password")) {
+        return next();
+    }
     
-},{timestamps:true , versionKey:false});
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+});
 
 const User = mongoose.model("User", UserModel);
-
-export default  User;
+export default User;
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import app from "./app.js";
 import { config } from "dotenv";
 import connectToDatabase from "./configs/connect.db.js";
 config();
-
+import User from "./models/users.model.js";
 const PORT = process.env.PORT || 3001;
 // Start server on the port defined in .env file or 3001 if not present
 
@@ -12,10 +12,33 @@ connectToDatabase()
         console.log(`Server is running on http://localhost:${PORT}`);
     });
     
+    init();
 }).catch((err) =>{
      console.log("Error connecting to database", err);
 });
 
+async function init (){
+    let user = await User.findOne({userId : "admin"});
+
+    if(user){
+         console.log("Admin user already present");
+         return ;
+    }
+
+    try {
+        user = await  User.create({
+            name : "Himanshu",
+            userId: "admin",
+            email : "hk225064@gmail.com",
+            password : "welcome1",
+            userType: "ADMIN"
+        })
+        
+        console.log(user);
+    } catch (error) {
+        console.log("Admin creation failed", error);
+    }
+}
 // Handle any errors that occur while starting up the server
 process.on("uncaughtException", (err) => {
     console.error("Uncaught Exception: ", err); 
